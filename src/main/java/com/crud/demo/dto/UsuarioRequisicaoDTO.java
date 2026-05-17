b0698e00e7f3b78d93e3be03f4bf3daee5ae7790
bispobr <156684382+bispobr@users.noreply.github.com>
2026-05-17 14:42:32 -0300

    feat: add dto de requisição

---
+++ src/main/java/com/crud/demo/dto/UsuarioRequisicaoDTO.java
@@ -0,0 +1,7 @@
+package com.crud.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UsuarioRequisicaoDTO(@Email String email,@NotBlank String nome) {
+}
